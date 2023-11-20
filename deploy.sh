3a951880dea7abc1d07037cf2a0c0e793cc72812
w0rp <devw0rp@gmail.com>
2023-11-20 00:29:54 +0000

    Update CD steps

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,10 @@ set -eu
 # Make sure we run this from the directory the script is in.
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
+# Pull the latest code.
+git pull
+# Update submodules such as the template.
+git submodule update --init
 # Build the Hugo image from scratch.
 image=$(docker build -q .)
 # Run Hugo.
